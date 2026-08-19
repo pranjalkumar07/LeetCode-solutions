d29ffdfc024287c6615576bc23bd7ae0c4deddf9
Pranjal Kumar <pranjalk13490@gmail.com>
2026-08-19 23:55:32 +0530

    Time: 0 ms (100%), Space: 16.8 MB (69.93%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,58 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        queue<pair<pair<int,int>, int>> q;
-        int fresh = 0;
-
-        for (int i = 0; i<n; i++) {
-            for (int j = 0; j<m; j++) {
-
-                if (grid[i][j] == 2)
-                    q.push({{i, j}, 0});
-
-                else if (grid[i][j] == 1)
-                    fresh++;
-            }
-        }
-
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-
-        int time = 0;
-        int cnt = 0;
-
-        while (!q.empty()) {
-
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int t = q.front().second;
-            q.pop();
-
-            time = max(time, t);
-
-            for (int i = 0; i < 4; i++) {
-
-                int nRow = row + delRow[i];
-                int nCol = col + delCol[i];
-
-                if (nRow >= 0 && nRow < n &&
-                    nCol >= 0 && nCol < m &&
-                    grid[nRow][nCol] == 1) {
-
-                    grid[nRow][nCol] = 2;
-                    cnt++;
-                    q.push({{nRow, nCol}, t + 1});
+        int m=grid.size();
+        int n=grid[0].size();
+        queue<pair<pair<int,int>, int>>q;
+        int vis[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }
+                else{
+                    vis[i][j]=0;
                 }
             }
         }
-
-        if (cnt != fresh)
-            return -1;
-
-        return time;
+        int tm=0;
+        int dRow[]={-1,0,+1,0};
+        int dCol[]={0,+1,0,-1};
+        while(!q.empty()){
+            int r= q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            tm=max(tm,t);
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=r+dRow[i];
+                int ncol=c+dCol[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+                q.push({{nrow,ncol},t+1});
+                vis[nrow][ncol]=2;
+            }
+        }
     }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(vis[i][j]!=2 && grid[i][j]==1){
+                return -1;
+            }
+        }
+    }
+    return tm;
+}
 };
